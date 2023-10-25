0da5471a682e9bd3fff012b2e4709929fc792f24
Betancur Mariano <125041706+MDB-Xeneize@users.noreply.github.com>
2023-10-25 10:57:32 -0300

    Update index.js
    Se eliminaron comentarios no utilizados

--- Backend/index.js
+++ Backend/index.js
@@ -11,8 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 morgan(':method :url :status :res[content-length] - :response-time ms');
 
-//const configuracion = require("config.json");
-//const controladorPersona = require("controller/personaController.js");
+
 const controladorUsuario = require("controller/usuarioController.js");
 const controladorChofer = require("controller/choferController.js");
 const controladorVehiculo = require("controller/vehiculoController.js");
@@ -20,9 +19,10 @@ const controladorMantenimiento = require("controller/mantenimientoController.js"
 const controladorTipo_Viaje = require("controller/tipo_ViajeController.js");
 const controladorViaje = require("controller/inOutController.js");
 const controladorGestiona = require("controller/gestionaController.js");
-//const securityController = require("controller/securityController.js");
+
+
 // Rutas de controladores
-//app.use('/api/persona', controladorPersona);
+
 const auth = require("config/auth.js");
 
 
@@ -34,7 +34,7 @@ app.use('/api/mantenimiento', controladorMantenimiento);
 app.use('/api/tipo_viaje', controladorTipo_Viaje);
 app.use('/api/inOut', controladorViaje);
 app.use('/api/gestiona', controladorGestiona);
-//app.use('/security', securityController.app);
+
 
 app.listen(configuracion.server.port, (err) => {
     if (err) {
